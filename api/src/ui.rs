08f36a9c0ed44c2c351b5df607b71acdc3feb79e
unknown <i.mD3lta@gmail.com>
2025-12-20 09:18:12 +0100

    add links on the home page

--- api/src/ui.rs
+++ api/src/ui.rs
@@ -1,8 +1,13 @@
+use std::fmt;
+use std::fmt::{Debug, Display, Formatter};
+use std::str::FromStr;
 use dioxus::fullstack::Form;
 use dioxus::prelude::*;
-use dioxus::router::RouterConfig;
-use crate::common::Credentials;
-use crate::login;
+use dioxus::router::{FromHashFragment, RouterConfig};
+use crate::common::{Credentials, ProfilID};
+use crate::{list_classes, login};
+
+
 
 #[rustfmt::skip]
 #[derive(Clone, Routable)]
@@ -11,16 +16,36 @@ pub enum Route {
     Home{},
     #[route("/login")]
     LoginPage{},
-    #[route("/class/:class_name#:profil_name")]
-    ClassPage{
-        class_name: String,
-        profil_name: u32
-    }
+    #[nest("/class/:class_name")]
+        #[route("")]
+        ClassPageDefault{
+            class_name: String,
+        },
+        #[route("/:id")]
+        ClassPageWithId {
+            class_name: String,
+            id: u32
+        }
+
 }
 
 #[component]
 pub fn Home() -> Element {
+
+    let classes = use_resource(list_classes);
+
     rsx! {
+        if let Some(classes) = classes() {
+            for class in classes?.iter() {
+                Link {
+                    to: Route::ClassPageDefault {
+                        class_name: class.clone(),
+                    },
+                    "{class}"
+                }
+            }
+        }
+
         //LoginBar {}
         div { "Home" }
     }
@@ -34,35 +59,46 @@ pub fn App() -> Element {
 }
 
 #[component]
-pub fn ClassPage(class_name: String, profil_name: String) -> Element {
+pub fn ClassPageDefault(class_name: String) -> Element {
+    rsx! {
+        ClassPage {
+            class_name: class_name,
+            profil_id: None
+        }
+    }
+}
+
+#[component]
+pub fn ClassPageWithId(class_name: String, id: u32) -> Element {
+    rsx! {
+        ClassPage {
+            class_name: class_name,
+            profil_id: Some(id)
+        }
+    }
+}
+
+#[component]
+pub fn ClassPage(class_name: String, profil_id: Option<u32>) -> Element {
     rsx! {
         "class name: {class_name}",
-        "profil name: {profil_name}"
     }
 }
 
 #[component]
 pub fn LoginPage() -> Element {
-    let mut fetch_login = use_action(login);
     rsx!(
         form {
             onsubmit: move |evt: FormEvent| async move {
-                // Prevent the browser from navigating away.
                 evt.prevent_default();
 
-                // Extract the form values into our `LoginForm` struct. The `.parsed_values` method
-                // is provided by Dioxus and works with any form element that has `name` attributes.
-                let values: Credentials = evt.parsed_values()?;
+                let values: Credentials = evt.parsed_values().expect("failed to parse values");
 
                 info!("trying to log");
 
-                // Call our server function with the form values wrapped in `Form`. The `SetHeader`
-                // response will set a cookie in the browser if the login is successful.
                 if login(Form(values)).await? {
                     navigator().replace(Route::Home {});
                 }
-
-                info!("logged");
                 Ok(())
             },
             input { r#type: "text", id: "name", name: "name" }
